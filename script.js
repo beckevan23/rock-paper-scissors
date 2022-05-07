cf82befcca3b958a81274169c5ec0ffe49de903c
nefarox <evan.beck23@gmail.com>
2022-05-07 13:21:18 -0500

    Add result divs, style page

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let choices = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
-let playChoice;
+let results = document.createElement('div');
+results.textContent = "||"
+let humanScore = document.createElement('div');
+let cpuScore = document.createElement('div');
+humanScore.classList.toggle('score');
+cpuScore.classList.toggle('score');
+humanScore.textContent = "Player Score: " + playerScore;
+cpuScore.textContent = "Computer Score: " + computerScore;
+/* let playChoice; */
 
 function compChoice() {
     let choice = choices[Math.floor(Math.random() * choices.length)];
@@ -10,43 +18,27 @@ function compChoice() {
 
 function playRound(playerChoice, computerChoice) {
     function stats() {
-        console.log("Player: " + playerChoice.toLowerCase());
-        console.log("Computer: " + computerChoice);
-        console.log("Player Score: " + playerScore);
-        console.log("Computer Score: " + computerScore);
+        humanScore.textContent = "Player Score: " + playerScore;
+        cpuScore.textContent = "Computer Score: " + computerScore;
     };
     if (playerChoice.toLowerCase() == computerChoice) {
         stats();
-        console.log(`%cIt is a draw, ${playerChoice.toLowerCase()} vs. ${computerChoice}`, "color: blue");
+        results.textContent = `It is a draw, ${playerChoice.toLowerCase()} vs. ${computerChoice}`;
     } else if (playerChoice.toLowerCase() == "rock" && computerChoice == "scissors" ||
-               playerChoice.toLowerCase() == "paper" && computerChoice == "rock" ||
-               playerChoice.toLowerCase() == "scissors" && computerChoice == "paper") {
+    playerChoice.toLowerCase() == "paper" && computerChoice == "rock" ||
+    playerChoice.toLowerCase() == "scissors" && computerChoice == "paper") {
         ++playerScore;
         stats();
-        console.log(`%cYou win, ${playerChoice.toLowerCase()} beats ${computerChoice}`, "color: green");
+        results.textContent = `You win, ${playerChoice.toLowerCase()} beats ${computerChoice}`;
     } else if (playerChoice.toLowerCase() == "rock" && computerChoice == "paper" ||
-               playerChoice.toLowerCase() == "paper" && computerChoice == "scissors" ||
-               playerChoice.toLowerCase() == "scissors" && computerChoice == "rock") {
-        ++computerScore;
-        stats();
-        console.log(`%cYou lose, ${computerChoice} beats ${playerChoice.toLowerCase()}`, "color: red");
-    } else {
-        console.log("That is not a valid input");
-    };
-};
-
-function game() {
-    for (playerScore && computerScore; playerScore < 5 && computerScore < 5;) {
-        playChoice = prompt("Enter rock, paper, or scissors", "Rock");
-        playRound(playChoice, compChoice());
-    };
-    if (playerScore == 5) {
-        console.log("YOU WON THE GAME!");
-    } else if (computerScore == 5) {
-        console.log("YOU LOST THE GAME!");
-    };
-};
-
+    playerChoice.toLowerCase() == "paper" && computerChoice == "scissors" ||
+    playerChoice.toLowerCase() == "scissors" && computerChoice == "rock") {
+                   ++computerScore;
+                   stats();
+                   results.textContent = `You lose, ${computerChoice} beats ${playerChoice.toLowerCase()}`;
+                };
+            };
+            
 let buttonContainer = document.createElement('div');
 let rockBtn = document.createElement('button');
 rockBtn.textContent = "Rock";
@@ -54,22 +46,37 @@ let paperBtn = document.createElement('button');
 paperBtn.textContent = "Paper";
 let scissorsBtn = document.createElement('button');
 scissorsBtn.textContent = "Scissors";
+buttonContainer.classList.toggle('buttonContainer');
 document.body.appendChild(buttonContainer);
 buttonContainer.appendChild(rockBtn);
 buttonContainer.appendChild(paperBtn);
 buttonContainer.appendChild(scissorsBtn);
+buttonContainer.insertBefore(results, rockBtn);
+buttonContainer.insertBefore(cpuScore, results);
+buttonContainer.insertBefore(humanScore, cpuScore);
 let buttons = document.querySelectorAll('button');
 let array = Array.from(buttons)
-console.log(array)
 array.forEach(button => button.addEventListener('click', function roundPlay() {
     if (playerScore < 5 && computerScore < 5) {
         playRound(button.textContent, compChoice())
         if (playerScore == 5) {
             button.removeEventListener('click', roundPlay);
-            console.log("YOU WON THE GAME!");
+            results.textContent = "YOU WON THE GAME!";
+            let refresh = document.createElement('button');
+            refresh.textContent = "PLAY AGAIN";
+            buttonContainer.appendChild(refresh);
+            refresh.addEventListener('click', function () {
+                window.location.reload();
+            });
         } else if (computerScore == 5) {
             button.removeEventListener('click', roundPlay);
-            console.log("YOU LOST THE GAME! (scrub)")
-            };
+            results.textContent = "YOU LOST THE GAME! (scrub)";
+            let refresh = document.createElement('button');
+            refresh.textContent = "PLAY AGAIN";
+            buttonContainer.appendChild(refresh);
+            refresh.addEventListener('click', function () {
+                window.location.reload();
+            });
+        };
     } 
 }));
